50edac8adc993d7787d544e3508b5ea1340f709e
124c4a11 <124c4a11@mail.ru>
2019-09-16 22:05:09 +0300

    SCSS: compiling when development

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const sassMiddleware = require('node-sass-middleware');
 
 
 const homeRoutes = require('./routes/home');
@@ -11,14 +10,6 @@ const coursesRoutes = require('./routes/courses');
 const app = express();
 app.set('view engine', 'pug');
 
-
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'scss'),
-  dest: path.join(__dirname, 'public', 'css'),
-  debug: true,
-  outputStyle: 'compressed',
-  prefix: '/css'
-}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
