f2976c2df0be4adbcece862883cbbd21e921c769
124c4a11 <124c4a11@mail.ru>
2019-10-06 00:43:08 +0300

    Protection for editing and deleting a course

--- routes/courses.js
+++ routes/courses.js
@@ -6,13 +6,21 @@ const auth = require('../middleware/auth');
 const Course = require('../models/Course');
 
 
+function isOwner(course, req) {
+  return course.userId.toString() === req.user._id.toString();
+}
+
+
 router.get('/', async (req, res) => {
   try {
-    const courses = await Course.find();
+    const courses = await Course.find()
+      .populate('userId', 'email name')
+      .select('price title img');
 
     res.render('courses', {
       title: 'Courses',
       courses,
+      userId: req.user ? req.user._id.toString() : null,
       isCourses: true
     });
   } catch (err) {
@@ -41,6 +49,8 @@ router.get('/:id/edit', auth, async (req, res) => {
   try {
     const course = await Course.findById(req.params.id);
 
+    if (!isOwner(course, req)) return res.redirect('/courses');
+
     res.render('course-edit', {
       title: 'Course edit',
       course
@@ -57,7 +67,13 @@ router.post('/edit', auth, async (req, res) => {
   delete req.body.id;
 
   try {
-    await Course.findByIdAndUpdate(id, req.body);
+    const course = await Course.findById(id);
+
+    if (!isOwner(course, req)) return res.redirect('/courses');
+
+    Object.assign(course, req.body);
+
+    await course.save();
 
     res.redirect('/courses');
   } catch (err) {
@@ -68,7 +84,10 @@ router.post('/edit', auth, async (req, res) => {
 
 router.post('/remove', auth, async (req, res) => {
   try {
-    await Course.deleteOne({ _id: req.body.id });
+    await Course.deleteOne({
+      _id: req.body.id,
+      userId: req.user._id
+    });
 
     res.redirect('/courses');
   } catch (err) {
